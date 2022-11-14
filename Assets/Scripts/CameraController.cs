0e7d7ad54e64dc4540b5592ff46ed7e2f1ab2648
Ricardo <rmachadobdev@gmail.com>
2022-11-14 18:08:45 -0300

    Física e camera
    Colocada física no personagem e câmera principal acompanhando o movimento do jogador

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 distanciaDoPlayer;
+   
+    void Start()
+    {
+        distanciaDoPlayer = transform.position - player.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = player.transform.position + distanciaDoPlayer;
+        
+    }
+}
--- Assets/Scripts/ControlaJogador.cs
+++ Assets/Scripts/ControlaJogador.cs
@@ -4,17 +4,14 @@ using UnityEngine;
 
 public class ControlaJogador : MonoBehaviour
 {
-
     public float velocidade = 10;
+    Vector3 direcao;
+
     void Update()
     {
         float eixoX = Input.GetAxis("Horizontal");
         float eixoZ = Input.GetAxis("Vertical");
-
-        Vector3 direcao = new Vector3(eixoX, 0, eixoZ);
-
-        transform.Translate(direcao * velocidade * Time.deltaTime);
-
+        direcao = new Vector3(eixoX, 0, eixoZ);
 
         if (direcao != Vector3.zero)
         {
@@ -24,5 +21,13 @@ public class ControlaJogador : MonoBehaviour
         {
             GetComponent<Animator>().SetBool("Movendo", false);
         }
+    }
+
+    //ao invés de rodar a cada frame, ele roda a cada 0,02s por padrão
+     void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition
+          (GetComponent<Rigidbody>().position +
+          (direcao * velocidade * Time.deltaTime));
     }
 }

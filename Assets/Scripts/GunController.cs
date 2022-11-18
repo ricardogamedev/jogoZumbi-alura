c31fade1e4fcbd12f117a212b8ddded910e45747
Ricardo <rmachadobdev@gmail.com>
2022-11-18 19:59:02 -0300

    commit

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public GameObject bala;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(bala, transform.position, transform.rotation);
+        }
+    }
+
+
+}

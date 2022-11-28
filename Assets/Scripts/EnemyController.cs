8a9c844afc79ddc446bd90a4e1d8e68522d2b2f5
Ricardo <rmachadobdev@gmail.com>
2022-11-28 17:01:58 -0300

    zumbi ataca jogador

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,6 +7,7 @@ public class EnemyController : MonoBehaviour
     public GameObject player;
     public float velocidade = 5;
 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,20 +23,28 @@ public class EnemyController : MonoBehaviour
     {
         float distancia = Vector3.Distance(transform.position, player.transform.position);
 
+        Vector3 direcao = player.transform.position - transform.position;
+
+        Quaternion novaRotacao = Quaternion.LookRotation(direcao);
+        GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+
         if (distancia > 2.5)
         {
-            Vector3 direcao = player.transform.position - transform.position;
 
             GetComponent<Rigidbody>().MovePosition
                 (GetComponent<Rigidbody>().position +
                direcao.normalized * velocidade * Time.deltaTime);
 
-            //Quaternions are used to represent rotations.
-            Quaternion novaRotacao = Quaternion.LookRotation(direcao);
+            //Quaternions are used to represent rotations. 
             GetComponent<Rigidbody>().MoveRotation(novaRotacao);
 
+            GetComponent<Animator>().SetBool("Atacando", false);
 
-                }
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Atacando", true);
+        }
     }
 
 

450ec64e0388bd4c6ba9bf229c244b3899f2b738
Ricardo <rmachadobdev@gmail.com>
2022-11-28 16:48:46 -0300

    Destruir os zumbis

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -7,10 +7,20 @@ public class BulletController : MonoBehaviour
 
     public float velocidade = 20;
     // fixed update porque vou usar um Rigidbody na bala
-    
+
     void FixedUpdate()
     {
-        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + 
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position +
             transform.forward * velocidade * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider objetoDeColisao)
+    {
+        if (objetoDeColisao.tag == "Inimigo")
+        {
+            Destroy(objetoDeColisao.gameObject);
+        }
+        //gameObject minúsculo pega quem está com o script 
+        Destroy(gameObject);
     }
 }

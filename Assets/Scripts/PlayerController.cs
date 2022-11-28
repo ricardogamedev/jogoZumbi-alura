0c364c518f60e1f821b8ac7ce2248ddf1238dfc3
Ricardo <rmachadobdev@gmail.com>
2022-11-28 12:10:58 -0300

    Rotacao do jogador

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float velocidade = 10;
     Vector3 direcao;
+    public LayerMask mascaraChao;
 
     void Update()
     {
@@ -29,5 +30,22 @@ public class PlayerController : MonoBehaviour
         GetComponent<Rigidbody>().MovePosition
             (GetComponent<Rigidbody>().position + 
             (direcao * velocidade * Time.deltaTime));
+
+        Ray raio = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(raio.origin, raio.direction * 100, Color.red);
+
+        RaycastHit impacto;
+
+        //quando eu uso um Raycasthit, eu preciso colocar esse out no parâmetro para avisar que vai entrar no if sem valor, mas lá dentro terá
+        if(Physics.Raycast(raio, out impacto, 100, mascaraChao))
+        {
+            Vector3 posicaoMiraJogador = impacto.point - transform.position;
+
+            posicaoMiraJogador.y = transform.position.y;
+
+            Quaternion novaRotacao = Quaternion.LookRotation(posicaoMiraJogador);
+
+            GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+        }
     }
 }
